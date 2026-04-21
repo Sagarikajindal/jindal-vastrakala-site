8b2e2e361e5e4bac23fd4c9ac86e993234c6711c
Sagarikajindal <71215350+Sagarikajindal@users.noreply.github.com>
2026-04-21 21:04:55 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import Hero from "@/components/shop/Hero";
+import Marquee from "@/components/shop/Marquee";
 import FeaturedProducts from "@/components/shop/FeaturedProducts";
 import CategoryGrid from "@/components/shop/CategoryGrid";
+import ConsultationBanner from "@/components/shop/ConsultationBanner";
+import YouTubeBanner from "@/components/shop/YouTubeBanner";
 import BrandStory from "@/components/shop/BrandStory";
+import PolicyLinks from "@/components/shop/PolicyLinks";
 import WhatsAppFloat from "@/components/shop/WhatsAppFloat";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Marquee />
       <CategoryGrid />
       <FeaturedProducts />
+      <ConsultationBanner />
+      <YouTubeBanner />
       <BrandStory />
+      <PolicyLinks />
       <WhatsAppFloat />
     </>
   );
