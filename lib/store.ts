94226f191d2c5653f313f68b50658753990f3417
Sagarikajindal <71215350+Sagarikajindal@users.noreply.github.com>
2026-04-21 21:57:56 +0200

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -2,7 +2,7 @@
 
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { CartItem, Product } from "./types";  
+import type { Product } from "./types";
 
 export interface CartItem {
   product: Product;
